189ad7b487028f1a51fc09fa17acbbe48a6a37a8
rudfol <33jhkim@gmail.com>
2025-03-02 02:32:10 +0000

    Update RGBColorCode.cpp

--- RGBColorCode.cpp
+++ RGBColorCode.cpp
@@ -12,8 +12,6 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    If you are interesting contact me: 33jhkim@gmail.com        :')
 */
 
 
